2878f57e06248bd521562014a4b449b73f8a38d4
xFuture603 <daniel.xfuture@googlemail.com>
2023-02-17 13:46:51 +0100

    added some further debugging

--- repo_scanner.py
+++ repo_scanner.py
@@ -118,6 +118,9 @@ def run_scan(
         check=False,
     )
 
+    logging.debug(scanner.stdout)
+    logging.debug(scanner.stderr)
+
     if scanner.returncode != 0:
         logging.info(
             "%s: returncode is %s - create a ticket", project_path, scanner.returncode
@@ -127,7 +130,7 @@ def run_scan(
         )
     else:
         projects_with_issues_fixed += close_issue(project=project)
-        logging.info("kics scan successful")
+        logging.info("%s: kics scan successful - did not find any leaked credentials.", project_path)
 
     return projects_with_issues_fixed, projects_with_issues_found
 
